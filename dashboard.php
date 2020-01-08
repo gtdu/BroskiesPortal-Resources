17f7e20bb19f6bc1bb94e3ed61fc5f47c5e4e093
Ryan Cobelli <ryan.cobelli@rybel-llc.com>
2020-01-07 22:51:01 -0500

    Remove container div

--- dashboard.php
+++ dashboard.php
@@ -43,14 +43,13 @@ if ($_SESSION['level'] > 1) {
 if ($_SESSION['level'] > 1) {
 
     ?>
-    <div class="container">
-        <h1 class="mt-2">Resources</h1>
-        <div class="d-flex mt-3 mb-3">
-            <div class="btn-group flex-fill" role="group" aria-label="Basic example">
-                <a href="?action=newResource" class="btn btn-warning">Create New Resource</a>
-                <a href="?action=deleteResource" class="btn btn-warning">Delete Resource</a>
-            </div>
+    <h1 class="mt-2">Resources</h1>
+    <div class="d-flex mt-3 mb-3">
+        <div class="btn-group flex-fill" role="group" aria-label="Basic example">
+            <a href="?action=newResource" class="btn btn-warning">Create New Resource</a>
+            <a href="?action=deleteResource" class="btn btn-warning">Delete Resource</a>
         </div>
+    </div>
     <?php
 
     if ($_GET['action'] == 'newResource') {
@@ -128,7 +127,6 @@ if (count($resources) == 0) {
 }
 
 ?>
-</div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script></body>
